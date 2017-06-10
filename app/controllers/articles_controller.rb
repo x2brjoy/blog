e3bcda0a5ed4ac1f2be7664cc2acf6f8696262b6
x2brjoy <x2brjoy.apc@gmail.com>
2017-06-10 17:40:41 -0500

    CRUD feature done and comment model added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,46 @@
 class ArticlesController < ApplicationController
 	# def new should come before def show
+	def index
+		@articles = Article.all
+	end
 	def new
+		@article = Article.new
 	end
 
-  	def show
-    	@article = Article.find(params[:id])
-  	end
+	def edit
+		@article = Article.find(params[:id])
+	end
 
 	def create
   		@article = Article.new(article_params)
  
-  		@article.save
-  		redirect_to @article
+  		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
 	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+  	def destroy
+    	@article = Article.find(params[:id])
+    	@article.destroy
+ 
+    	redirect_to articles_path
+  	end
+
+	def show
+    	@article = Article.find(params[:id])
+  	end
 
 	private
   		def article_params
